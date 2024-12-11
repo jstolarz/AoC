9a1b44b50f16a0ecab33087f4ce12f6bb4260aa2
Jedrzej Stolarz <jedrzej.stolarz@nokia.com>
2024-12-11 16:07:41 +0100

    Day 11

--- AOC2024/day11/p1.py
+++ AOC2024/day11/p1.py
@@ -1,18 +1,26 @@
+from functools import cache
+
 with open("input") as f:
     stones = [int(i) for i in f.read().split()]
     
 BLINKS = 25
 
+@cache
+def blink_on_stone(stone):
+    if stone == 0:
+        return [1]
+    elif (l:=len(str(stone))) % 2 ==0:
+        left, right = str(stone)[:l//2], str(stone)[l//2:]
+        return [int(left), int(right)] 
+    else:
+        return [stone*2024]
+    
+
 for i in range(BLINKS):
+    print(i)
     new_stones = []
     for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-        elif (l:=len(str(stone))) % 2 ==0:
-            left, right = str(stone)[:l//2], str(stone)[l//2:]
-            new_stones.extend([int(left), int(right)])
-        else:
-            new_stones.append(stone*2024)
+        new_stones += blink_on_stone(stone)
     stones = new_stones
 
 print(len(stones))
